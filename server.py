0e63cf92b76ab327903b72b1be871ad171c1f54c
russelltran <45905880+Russell-Tran@users.noreply.github.com>
2019-08-01 19:00:22 -0700

    Fix flask

--- server.py
+++ server.py
@@ -3,7 +3,9 @@
 "otherwise, Heroku will think your app isn't working and it will be in the 'crashed' state"
 """
 
-from os import environ
+#from os import environ
+
+import os
 from flask import Flask
 
 app = Flask(__name__)
