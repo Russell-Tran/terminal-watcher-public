2431af7c5752844802134135e31e2ca720f8d1c7
russelltran <45905880+Russell-Tran@users.noreply.github.com>
2019-08-01 18:59:06 -0700

    Fixing flask server

--- server.py
+++ server.py
@@ -7,4 +7,10 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
-app.run(environ.get('PORT'))
+
+
+# app.run(environ.get('PORT'))
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
+
+print("Hello world I love you!")
