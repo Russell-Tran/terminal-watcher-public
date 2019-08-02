648a7cb114b7c2282c3bafcb3e08eab7417bbd77
russelltran <45905880+Russell-Tran@users.noreply.github.com>
2019-08-01 18:42:23 -0700

    Send texts as part of refresh sequence

--- aquavian_refresh_datetime.py
+++ aquavian_refresh_datetime.py
@@ -4,6 +4,7 @@ Aquavian Freight company's Containers data table.
 It updates it to now.
 """
 import tadabase_practice # local file "tadabase_practice.py"
+import twilio_practice # local file "twilio_practice.py"
 
 # tadabase_practice.print_all_tables_in_app()
 # Output: ...{"id":"lGArg7rmR6","name":"Containers"}...
@@ -14,7 +15,11 @@ aquavian_containers_table_id = "lGArg7rmR6"
 aquavian_datetime_field_id = "field_79"
 
 if __name__ == '__main__':
+	now_string = tadabase_practice.generate_now_string()
 	print("The time is:")
-	print(tadabase_practice.generate_now_string())
+	print(now_string)
+
 	tadabase_practice.update_all_datetime_now(table_id=aquavian_containers_table_id, 
 		field_id=aquavian_datetime_field_id)
+
+	twilio_practice.quick_text("Aquavian containers datetime updated at " + now_string)
